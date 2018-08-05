6aaf135ffe332af4c00a90f8ddea914bac50eef8
Nicholas Suski <nick@suskitech.org>
2018-08-05 12:19:26 -0600

    Remove version from dist in favor of github releases

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = [
       },
       output: {
           path: path.resolve(__dirname, 'dist'),
-          filename: `NEWT-${process.env.npm_package_version}.min.js`,
+          filename: `NEWT.min.js`,
           library: 'NEWT',
           libraryTarget: 'umd',
           umdNamedDefine: true
